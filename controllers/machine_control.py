18712c10707d4f90789e158f42e4eb9880f3771e
einarorn228 <einarorn864@gmail.com>
2024-10-07 00:05:42 +0000

    Adding a database and event-driven QR code scanning, plus other improvements

---
+++ controllers/machine_control.py
@@ -0,0 +1,12 @@
+def start_machine():
+    """Logic to start the washing machine."""
+    print("Starting the washing machine...")
+
+def stop_machine():
+    """Logic to stop the washing machine."""
+    print("Stopping the washing machine...")
+
+def check_machine_status():
+    """Logic to check the status of the washing machine."""
+    # You could send a request to the washing machine to get its current status
+    return "Idle"
